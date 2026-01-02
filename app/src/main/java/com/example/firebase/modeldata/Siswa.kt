b1e82c7001506d099dee8f21aa997a695ed17eb8
alifinrayhan <alifinrayhan@gmail.com>
2026-01-02 15:44:17 +0700

    memperbaruin data class Siswa dan menambahkan data class Detailsiswa

--- app/src/main/java/com/example/firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/firebase/modeldata/Siswa.kt
@@ -4,5 +4,15 @@ data class Siswa(
     val id: Long = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telepon: String = "",
+    val telpon: String = "",
 )
+
+data class DetailSiswa(
+    val id: Long,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
