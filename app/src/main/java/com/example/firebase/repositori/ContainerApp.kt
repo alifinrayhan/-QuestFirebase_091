16d3a8f7e49bd334f1afbba0f5db50739c02a328
alifinrayhan <alifinrayhan@gmail.com>
2026-01-02 16:08:08 +0700

    menambahkan interface cintainerApp, class dafaultcontainerapp, class apliasi data siswa

---
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -0,0 +1,23 @@
+package com.example.firebase.repositori
+
+import android.app.Application
+
+
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        FirebaseRepositoriSiswa()
+    }
+
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCrate() {
+        super.onCreate()
+        this.container = DefaultContainerApp
+    }
+}
